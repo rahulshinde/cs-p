1acba7f07166dbccdc7d3ce04fc01a1186f1f42f
Rahul Shinde <rshinde@mica.edu>
2016-09-03 15:13:31 -0600

    blur on scroll

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 $( document ).ready(function() {
 	$('.draggable').each(function() {
 		for (i = 0; i <= 2; i++) { 
-    		RandomPlace(i);
+    		randomPlace(i);
 		}
 	});
 
 	$(".draggable").fadeToggle();
 
-    Draggable();
+	sizeContent()
+    draggable();
+
 });
 
-var docHeight = $(document).height() - 100,
-    docWidth = $(document).width(),
+// Randomly place links
+
+var docHeight = $(window).height() - 100,
+    docWidth = $(window).width(),
     $div = $('.draggable'),
     divWidth = $div.width(),
     divHeight = $div.height(),
@@ -19,13 +23,43 @@ var docHeight = $(document).height() - 100,
     widthMax = docWidth - divWidth;
 
 
-function RandomPlace(i) {
+function randomPlace(i) {
 	var top = Math.floor( Math.random() * heightMax ),
 		left = Math.floor( Math.random() * widthMax );
 
     $('#' + i).css({"position":"absolute","top": top + "px", "left": left + "px"});
 }
 
-function Draggable() {
+function draggable() {
     $('.draggable').draggable();
 }
+
+// Link hover states
+
+function hover(element) {
+	var source = element.getAttribute('src');
+	source = source.replace('.png', '_hover.png')
+    element.setAttribute('src', source);
+}
+function unhover(element) {
+	var source = element.getAttribute('src');
+	source = source.replace('_hover.png', '.png')
+    element.setAttribute('src', source);
+}
+
+// Info Panel
+
+$(window).resize(sizeContent);
+
+//Dynamically assign height
+function sizeContent() {
+	var newHeight = $(window).height() + "px";
+	$(".info").css("top", newHeight);
+}
+
+$(window).scroll(function () {
+	var oVal;
+	oVal = $(window).scrollTop() / 200;
+	console.log(oVal);
+	return $('.background_blur').css('opacity', oVal);
+});
