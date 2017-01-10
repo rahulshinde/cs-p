d0b111ecce24f2801ed75810d8c5490434216641
Rahul Shinde <rshinde@mica.edu>
2017-01-10 13:54:20 -0500

    adding new images

--- js/main.js
+++ js/main.js
@@ -1,65 +1,11 @@
-$( document ).ready(function() {
-	$('.draggable').each(function() {
-		for (i = 0; i <= 2; i++) { 
-    		randomPlace(i);
-		}
-	});
+$(document).ready( function(){
+  // root size
+  root_size = $(window).width() / 76.8;
+  $('html').css({ 'font-size': root_size });
+  $(window).resize(changeRootFontSize);
+})
 
-	$(".draggable").fadeToggle();
-
-	sizeContent()
-    draggable();
-
-});
-
-// Randomly place links
-
-var docHeight = $(window).height() - 100,
-    docWidth = $(window).width(),
-    $div = $('.draggable'),
-    divWidth = $div.width(),
-    divHeight = $div.height(),
-    heightMax = docHeight - divHeight,
-    widthMax = docWidth - divWidth;
-
-
-function randomPlace(i) {
-	var top = Math.floor( Math.random() * heightMax ),
-		left = Math.floor( Math.random() * widthMax );
-
-    $('#' + i).css({"position":"absolute","top": top + "px", "left": left + "px"});
+changeRootFontSize = function() {
+  root_size = $(window).width() / 76.8;
+  $('html').css({ 'font-size': root_size });
 }
-
-function draggable() {
-    $('.draggable').draggable();
-}
-
-// Link hover states
-
-function hover(element) {
-	var source = element.getAttribute('src');
-	source = source.replace('.png', '_hover.png')
-    element.setAttribute('src', source);
-}
-function unhover(element) {
-	var source = element.getAttribute('src');
-	source = source.replace('_hover.png', '.png')
-    element.setAttribute('src', source);
-}
-
-// Info Panel
-
-$(window).resize(sizeContent);
-
-//Dynamically assign height
-function sizeContent() {
-	var newHeight = $(window).height() + "px";
-	$(".info").css("top", newHeight);
-}
-
-$(window).scroll(function () {
-	var oVal;
-	oVal = $(window).scrollTop() / 200;
-	console.log(oVal);
-	return $('.background_blur').css('opacity', oVal);
-});
